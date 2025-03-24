c64874eddde34a6100b5f6dcf869aa475b0de1e5
martingom4 <martingomez9636@gmail.com>
2025-03-23 19:40:11 -0500

    puesta de productos dinamicamente

--- src/components/Ui/UiProducts.tsx
+++ src/components/Ui/UiProducts.tsx
@@ -1,17 +1,5 @@
 import { useState } from 'react';
-import {motion} from 'motion/react' ;
-
-const initialProducts = [
-  {
-    id: 1,
-    name: 'Hamburguesa Premium',
-    description: 'Carne Angus, queso cheddar, tocino, salsa BBQ',
-    price: 1.99,
-    imageUrl: 'https://www.recetasnestle.com.ec/sites/default/files/styles/recipe_detail_desktop_new/public/srh_recipes/4e4293857c03d819e4ae51de1e86d66a.jpg?itok=F7KRSXDy',
-  },
-
-  // Puedes añadir más aquí
-];
+import { products as initialProducts } from '../../handlers/productsHandler';
 
 export default function ProductsList() {
   const [products] = useState(initialProducts);
@@ -21,7 +9,7 @@ export default function ProductsList() {
       <h2 className="text-2xl font-bold mb-4">Nuestros Productos</h2>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {products.map((product) => (
-    
+
             <div key={product.id} className="bg-primary-red shadow-xl rounded-xl overflow-hidden">
                 <img src={product.imageUrl} alt={product.name} className="w-full h-48 object-cover" />
                 <div className="p-4">
---
+++ src/handlers/productsHandler.ts
@@ -0,0 +1,16 @@
+type Products = {
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    imageUrl: string;
+}
+export const products:Products[]= [
+    {
+        id: 1,
+        name: 'Hamburguesa Premium',
+        description: 'Carne Angus, queso cheddar, tocino, salsa BBQ',
+        price: 1.99,
+        imageUrl: 'https://www.recetasnestle.com.ec/sites/default/files/styles/recipe_detail_desktop_new/public/srh_recipes/4e4293857c03d819e4ae51de1e86d66a.jpg?itok=F7KRSXDy',
+      },
+];
