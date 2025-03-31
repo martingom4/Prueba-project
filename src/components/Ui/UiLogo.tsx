127d4de44038edcdb989894a65067bfcf866fd09
martingom4 <martingomez9636@gmail.com>
2025-03-30 21:22:09 -0500

    intento de componente rect

--- src/components/Ui/UiLogo.tsx
+++ src/components/Ui/UiLogo.tsx
@@ -5,7 +5,11 @@ export default function Uilogo(){
     return (
         <div>
             <Link to="/">
-
+                <img
+                    src="../../images/Logo.png"
+                    alt="Logo"
+                    className="h-12 w-12 rounded-full object-cover"
+                />
 
             </Link>
         </div>
